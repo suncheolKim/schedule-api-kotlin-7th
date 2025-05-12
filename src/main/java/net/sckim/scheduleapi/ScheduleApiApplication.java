06eb4eba4f57b8d1a82babbfbd3dad0a190e5ace
Suncheol Kim <tinymin@yahoo.co.kr>
2025-05-12 16:07:36 +0900

    일정 관리 앱 최초 커밋

---
+++ src/main/java/net/sckim/scheduleapi/ScheduleApiApplication.java
@@ -0,0 +1,13 @@
+package net.sckim.scheduleapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/sckim/scheduleapi/ScheduleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.sckim.scheduleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
